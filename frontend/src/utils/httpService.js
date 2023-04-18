3bdbad18d264eecdb5eb282be0630cacb05dc3eb
Justice Nyaga <justiceevansmwema@gmail.com>
2023-04-18 21:20:29 +0300

    change production api url

--- frontend/src/utils/httpService.js
+++ frontend/src/utils/httpService.js
@@ -4,7 +4,7 @@ const getBaseUrl = () => {
   let url;
   switch (process.env.REACT_APP_API_URL) {
     case "production":
-      url = "http://127.0.0.1:8000";
+      url = "https://bridge-app-qcn53.ondigitalocean.app";
       break;
     case "development":
     default:

4dd47a4195040fba4f82feed2cc933519462784d
Justice Nyaga <justiceevansmwema@gmail.com>
2023-04-18 21:20:29 +0300

    change production api url

--- frontend/src/utils/httpService.js
+++ frontend/src/utils/httpService.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const getBaseUrl = () => {
-  let url;
-  switch (process.env.REACT_APP_API_URL) {
-    case "production":
-      url = "http://127.0.0.1:8000";
-      break;
-    case "development":
-    default:
-      url = "http://127.0.0.1:8000";
-  }
+// const getBaseUrl = () => {
+//   let url;
+//   switch (process.env.REACT_APP_API_URL) {
+//     case "production":
+//       url = "https://bridge-app-qcn53.ondigitalocean.app";
+//       break;
+//     case "development":
+//     default:
+//       url = "http://127.0.0.1:8000";
+//   }
 
-  return url;
-};
+//   return url;
+// };
 
-axios.defaults.baseURL = getBaseUrl();
+// axios.defaults.baseURL = getBaseUrl();
 
 export default {
   request: axios.request,
